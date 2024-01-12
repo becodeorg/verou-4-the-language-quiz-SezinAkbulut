7ba02f36721d94639b3fdebe9b3fee040084577d
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-12 15:12:28 +0100

    updat index.php

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class LanguageGame
 {
     private $words;
--- classes/player.php
+++ classes/player.php
@@ -1,15 +1,11 @@
 <?php
 
-
 class Player
 {
     // TODO: add name and score
-    private $name;
-    private $score;
 
     public function __construct()
     {
         // TODO: add 👤 automatically to their name
     }
 }
-
