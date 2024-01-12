7d0fbc4a285bea9291085c424c9f2d5a5cd26cbc
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-12 15:31:33 +0100

    add functions for start the game

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -11,19 +11,57 @@ class LanguageGame
         // and are used mostly for more *static* types of data (a fixed set of translations in this case)
         foreach (Data::words() as $frenchTranslation => $englishTranslation) {
             // TODO: create instances of the Word class to be added to the words array
+            $this->words[] = new Word($frenchTranslation, $englishTranslation);
         }
     }
 
+    // TODO: check for option A or B
+    // Option A: user visits site first time (or wants a new word)
     public function run()
     {
-        // TODO: check for option A or B
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->processAnswer();
+        } else {
+            $this->selectRandomWord();
+        }
+    }
 
-        // Option A: user visits site first time (or wants a new word)
-        // TODO: select a random word for the user to translate
+    // TODO: select a random word for the user to translate
+    private function selectRandomWord()
+    {
+        if (!empty($this->words)) {
+            $randomKey = array_rand($this->words);
+            $randomWord = $this->words[$randomKey];
 
-        // Option B: user has just submitted an answer
-        // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
-        // TODO: generate a message for the user that can be shown
+            session_start();
+            $_SESSION['random_word'] = $randomWord;
 
+            // Display the random word for translation in the HTML form
+            echo "Translate the word: {$randomWord->getFrenchTranslation()}";
+        } else {
+            echo "No words available for translation.";
+        }
+    }
+
+
+    // Option B: user has just submitted an answer
+    private function processAnswer()
+    {
+        session_start();
+
+        if (isset($_SESSION['random_word'])) {
+            $randomWord = $_SESSION['random_word'];
+            $userAnswer = $_POST['user_answer'];
+
+            //TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
+            if ($randomWord->verify($userAnswer)) {
+                // TODO: generate a message for the user that can be shown
+                echo "Correct! Well done.";
+            } else {
+                echo "Incorrect. Try again.";
+            }
+        } else {
+            echo "Session variable 'random_word' not set.";
+        }
     }
 }
--- classes/word.php
+++ classes/word.php
@@ -4,11 +4,33 @@
 class Word
 {
     // TODO: add word (FR) and answer (EN) - (via constructor or not? why?)
+    private $frenchTranslation;
+    private $englishTranslation;
 
+    public function __construct(string $frenchTranslation, string $englishTranslation)
+    {
+        $this->frenchTranslation = $frenchTranslation;
+        $this->englishTranslation = $englishTranslation;
+    }
+// TODO: use this function to verify if the provided answer by the user matches the correct one
     public function verify(string $answer): bool
     {
         // TODO: use this function to verify if the provided answer by the user matches the correct one
-        // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
-        // Bonus (hard): can you allow answers with small typo's (max one character different)?
+        $answerLower = strtolower($answer);
+        $correctAnswerLower = strtolower($this->englishTranslation);
+
+        //Bonus: Allow answers with different casing and small typo's (max one character different)
+        return $answerLower === $correctAnswerLower || levenshtein($answerLower, $correctAnswerLower) <= 1;
+    }
+
+    public function getFrenchTranslation(): string
+    {
+        return $this->frenchTranslation;
+    }
+
+    // Additional getter methods if needed
+    public function getEnglishTranslation(): string
+    {
+        return $this->englishTranslation;
     }
 }
