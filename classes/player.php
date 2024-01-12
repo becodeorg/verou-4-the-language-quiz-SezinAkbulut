db7b66c482c8023f8abcaa401f6c5378bccc4b02
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-12 15:49:23 +0100

    add name and score inside player file

--- classes/player.php
+++ classes/player.php
@@ -3,9 +3,14 @@
 class Player
 {
     // TODO: add name and score
+    private $name;
+    private $score;
 
     public function __construct()
     {
         // TODO: add 👤 automatically to their name
+        $this->name = "👤";
+        $this->score = 0;
     }
+
 }
